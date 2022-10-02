3af6c6d5b283f9d834bafc30d161495496bf09ce
Raphael Alexei <raphaeeelbr@gmail.com>
2022-10-02 00:42:50 -0300

    Weather command added
    Added weather command to check temperature of city

--- alexa.py
+++ alexa.py
@@ -4,9 +4,16 @@ import pyttsx3
 import datetime
 import wikipedia
 import pywhatkit
+import requests
+import json
+import dotenv
+import os
 
+
+dotenv.load_dotenv(dotenv.find_dotenv())
 audio = sr.Recognizer()
 machine = pyttsx3.init()
+ANGRY = 0
 
 
 class Answer:
@@ -22,12 +29,17 @@ answers = \
     Answer("namorar", "Não posso namorar, sou apenas um robô"), \
     Answer("angola", "É um local que carece de fome, nunca viram uma feijoada")
 
+def get_weather(city):
+    city = str(city).capitalize()
+    web = requests.get("https://api.openweathermap.org/data/2.5/weather?q=" + city + ",BR&appid=" + os.getenv("WEATHER_API") + "&lang=pt_br&units=metric")
+    content = json.loads(web.content)
+    return "A temperatura em " + city + " é de " + str(int(content["main"]["temp"])) + " graus, com máxima de " + str(int(content["main"]["temp_max"])) + " graus e mínima de " + str(int(content["main"]["temp_min"])) + " graus"
 
 def get_answer():
     try:
         with sr.Microphone() as source:
             print("Pode falar...")
-            listen = audio.listen(source, timeout=3)
+            listen = audio.listen(source, timeout=3, phrase_time_limit=5)
             command = audio.recognize_google(listen, language='pt-BR')
             command = command.lower()
             if 'júlia' in command:
@@ -40,6 +52,15 @@ def get_answer():
         print(e)
         return "Error"
 
+def get_price(value):
+    web = requests.get("http://economia.awesomeapi.com.br/json/last/" + value)
+    content = json.loads(web.content)
+    if "BTC" in value:
+        value = content[value.replace("-","")]["ask"]
+    else:
+        value = "%.2f" % float(content[value.replace("-","")]["ask"])
+    return "O valor está em R$ " + value
+
 
 def wikipedia_search(command):
     try:
@@ -47,11 +68,12 @@ def wikipedia_search(command):
         answer = wikipedia.summary(command, 2)
     except Exception as e:
         print(e)
-        return "Error"
-    return answer;
+        return "Não sei nada sobre isso"
+    return answer
 
 
 def listen_user():
+    global ANGRY
     command = get_answer()
     if "Error" in command: return
 
@@ -69,15 +91,27 @@ def listen_user():
         machine.runAndWait()
     else:
         if "quem é" in command:
-            answer = wikipedia_search(command)
+            answer = wikipedia_search(command.split("é")[1])
         elif "oque é" in command:
-            answer = wikipedia_search(command)
+            answer = wikipedia_search(command.split("é")[1])
         elif "como é" in command:
-            answer = wikipedia_search(command)
+            answer = wikipedia_search(command.split("é")[1])
         elif "sabe quem" in command:
-            answer = wikipedia_search(command)
+            answer = wikipedia_search(command.split("quem")[1])
         elif "o que é" in command:
-            answer = wikipedia_search(command)
+            answer = wikipedia_search(command.split("é")[1])
+        elif "dólar" in command:
+            answer = get_price("USD-BRL")
+        elif "euro" in command:
+            answer = get_price("EUR-BRL")
+        elif "bitcoin" in command:
+            answer = get_price("BTC-BRL")
+        elif "libra" in command:
+            answer = get_price("GBP-BRL")
+        elif "temperatura em" in command:
+            answer = get_weather(command.split(" em ")[1])
+        else:
+            answer = "Não sei nada sobre isso"
 
         for obj in answers:
             if obj.identify in command:
@@ -85,6 +119,12 @@ def listen_user():
                 break
 
         print(answer)
+        if "Não sei nada sobre isso" in answer:
+            ANGRY = ANGRY + 1
+            if(ANGRY > 2):
+                answer = "Oh porra, Eu ja te disse mil vezes que não sei"
+                ANGRY = 0
+        
         machine.say(answer)
         machine.runAndWait()
 
